9ce0eafd48a205f2ae9501746787b0cd905eaedd
Raul Lize Teixeira <lizeraul1@gmail.com>
2025-11-05 01:53:45 -0300

    feat: implement color list input and sorting functionality
    add interactive color input using readline-sync and display sorted results

--- atividades-praticas/atividade-4/ex1.ts
+++ atividades-praticas/atividade-4/ex1.ts
@@ -1,1 +1,23 @@
-console.log("Hello, TypeScript!");
+import readlinesync = require("readline-sync")
+
+const cores: Array<string> = new Array<string>(3)
+
+for (let i = 0; i <= cores.length - 1; i++) {
+    cores[i] = readlinesync.question('Digite uma cor: ')
+}
+
+console.log(`\nListar todas as cores:\n`)
+
+for (let cor of cores) {
+    console.log(cor)
+}
+
+console.log(`\nOrdenar as cores:\n`)
+
+cores.sort()
+
+for (let cor of cores) {
+    console.log(cor)
+}
+
+// console.log(cores)
